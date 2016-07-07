a17e9a90570cd601153fc0a147ba14786612a552
Huy Truong <HT@Huys-MacBook-Pro.local>
2016-07-07 09:33:09 -0700

    initial commmit

--- snapHack/ViewController.swift
+++ snapHack/ViewController.swift
@@ -6,14 +6,55 @@
 //  Copyright © 2016 Huy Truong. All rights reserved.
 //
 
+import AVFoundation
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController{
+    
+    let captureSession = AVCaptureSession()
+    
+    var captureDevice: AVCaptureDevice?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        captureSession.sessionPreset = AVCaptureSessionPresetLow
+        
+        let devices = AVCaptureDevice.devices()
+        
+        for device in devices{
+            if(device.hasMediaType(AVMediaTypeVideo)){
+                if(device.position == AVCaptureDevicePosition.Back){
+                    captureDevice = device as? AVCaptureDevice
+                }
+            }
+        }
+        
+        if captureDevice != nil {
+                beginSession()
+        }
     }
+    
+    func beginSession(){
+        var err: NSError? = nil
+        
+        //captureSession.addInput(AVCaptureDeviceInput(device: captureDevice, error: &err))
+        
+        do {
+            let input = try AVCaptureDeviceInput(device: captureDevice)
+            captureSession.addInput(input)
+        } catch {
+           print("error: ")
+        }
+        
+    
+        
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        self.view.layer.addSublayer(previewLayer)
+        previewLayer?.frame = self.view.layer.frame
+        captureSession.startRunning()
+    }
+   
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
