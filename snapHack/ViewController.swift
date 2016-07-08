a4d3f84e017949982e1be88ef1a992f8cc3c85b4
Huy Truong <HT@Huys-MacBook-Pro.local>
2016-07-08 10:55:47 -0700

    everything is work fine

--- snapHack/ViewController.swift
+++ snapHack/ViewController.swift
@@ -15,8 +15,14 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     @IBOutlet weak var pickFromGallergy: UIButton!
     
+    
+    var emojiArray : [UIButton] = []
+    
     @IBAction func pickFromGallery(sender: AnyObject) {
-        
+        for emoji in emojiArray {
+            emoji.removeFromSuperview()
+        }
+        emojiArray.removeAll()
         let imagePickerController = UIImagePickerController()
         imagePickerController.delegate = self
         imagePickerController.sourceType = UIImagePickerControllerSourceType.SavedPhotosAlbum
@@ -26,7 +32,71 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         })
     }
     
+    func addEmoji2(){
+        let ciImage  = CIImage(CGImage:imageView.image!.CGImage!)
+        let ciDetector = CIDetector(ofType:CIDetectorTypeFace
+            ,context:nil
+            ,options:[
+                CIDetectorAccuracy:CIDetectorAccuracyHigh,
+                CIDetectorSmile:true
+            ]
+        )
+        let features = ciDetector.featuresInImage(ciImage)
+        
+        UIGraphicsBeginImageContext(imageView.image!.size)
+        imageView.image!.drawInRect(CGRectMake(0,0,imageView.image!.size.width,imageView.image!.size.height))
+        
+        for feature in features{
+            
+            //context
+            let drawCtxt = UIGraphicsGetCurrentContext()
+            
+            //face
+            var faceRect = (feature as! CIFaceFeature).bounds
+            faceRect.origin.y = imageView.image!.size.height - faceRect.origin.y - faceRect.size.height
+            CGContextSetStrokeColorWithColor(drawCtxt, UIColor.redColor().CGColor)
+            CGContextStrokeRect(drawCtxt,faceRect)
+            
+            //mouse
+            if (feature as! CIFaceFeature).hasMouthPosition != false{
+                let mouseRectY = imageView.image!.size.height - (feature as! CIFaceFeature).mouthPosition.y
+                let mouseRect  = CGRectMake((feature as! CIFaceFeature).mouthPosition.x - 5,mouseRectY - 5,10,10)
+                CGContextSetStrokeColorWithColor(drawCtxt,UIColor.blueColor().CGColor)
+                CGContextStrokeRect(drawCtxt,mouseRect)
+            }
+            
+            //hige
+            let higeImg      = UIImage(named:"hige_100.png")
+            let mouseRectY = imageView.image!.size.height - (feature as! CIFaceFeature).mouthPosition.y
+            //ヒゲの横幅は顔の4/5程度
+            let higeWidth  = faceRect.size.width * 4/5
+            let higeHeight = higeWidth * 0.3 // 元画像が100:30なのでWidthの30%が縦幅
+            let higeRect  = CGRectMake((feature as! CIFaceFeature).mouthPosition.x - higeWidth/2,mouseRectY - higeHeight/2,higeWidth,higeHeight)
+            CGContextDrawImage(drawCtxt,higeRect,higeImg!.CGImage)
+            
+            //leftEye
+            if(feature as! CIFaceFeature).hasLeftEyePosition != false{
+                let leftEyeRectY = imageView.image!.size.height - (feature as! CIFaceFeature).leftEyePosition.y
+                let leftEyeRect  = CGRectMake((feature as! CIFaceFeature).leftEyePosition.x - 5,leftEyeRectY - 5,10,10)
+                CGContextSetStrokeColorWithColor(drawCtxt, UIColor.blueColor().CGColor)
+                CGContextStrokeRect(drawCtxt,leftEyeRect)
+            }
+            
+            //rightEye
+            if (feature as! CIFaceFeature).hasRightEyePosition != false{
+                let rightEyeRectY = imageView.image!.size.height - (feature as! CIFaceFeature).rightEyePosition.y
+                let rightEyeRect  = CGRectMake((feature as! CIFaceFeature).rightEyePosition.x - 5,rightEyeRectY - 5,10,10)
+                CGContextSetStrokeColorWithColor(drawCtxt, UIColor.blueColor().CGColor)
+                CGContextStrokeRect(drawCtxt,rightEyeRect)
+            }
+        }
+        let drawedImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        imageView.image = drawedImage
+    }
+    
     func addEmoji(){
+        let ciImage  = CIImage(CGImage:imageView.image!.CGImage!)
         let detector = CIDetector(
             ofType: CIDetectorTypeFace,
             context: nil,
@@ -34,30 +104,125 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         )
         
         let faces = detector.featuresInImage(
-            CIImage(image: imageView.image!)!,
+            ciImage,
             options: [ CIDetectorSmile: true ]
             ) as! [CIFaceFeature]
+//        print("inside addEmoji and faces.length is :\(faces.count)")
+        
+//        let features = detector.featuresInImage(ciImage)
+        
+        
+        
+        
+        UIGraphicsBeginImageContext(imageView.image!.size)
+        imageView.image!.drawInRect(CGRectMake(0,0,imageView.image!.size.width,imageView.image!.size.height))
         
         for face in faces {
             print(face.bounds)
+            
+            //context
+            let drawCtxt = UIGraphicsGetCurrentContext()
+            
+            
+            
+            
+//            print(resized)
+//            
 //            let blackView = UIView(frame: face.bounds)
 //            blackView.backgroundColor = UIColor.blackColor()
-//            imageView.addSubview(blackView)
-            let calloutView = UIButton(frame: face.bounds)
-            let text = face.hasSmile ? "😀" : "😐"
-            calloutView.titleLabel!.font = UIFont.systemFontOfSize(200)
-            calloutView.setTitle(text, forState: UIControlState.Normal)
+//            self.view.addSubview(blackView)
+//
+//            let times = face.bounds.width / imageView.frame.width
+//            let width = face.bounds.width / times
+//            let width = resized.size.width
+//
+//            let heightTimes = face.bounds.height / imageView.frame.height
+//            let height = face.bounds.height / heightTimes
+//            let height = resized.size.height
+//
+//            let xTimes = face.bounds.origin.x /  imageView.frame.origin.x
+//            let x = face.bounds.origin.x / xTimes
+           // let x  = resized.siz
             
-            imageView.addSubview(calloutView)
+//            
+//            let yTimes = face.bounds.origin.y / imageView.frame.origin.y
+//            let y = face.bounds.origin.y / yTimes
+            //let y = face.bounds.origin.y
+            
+          
+            
+            
+            var rect = face.bounds
+            
+            rect.origin.y = imageView.image!.size.height - rect.origin.y - rect.size.height
+            
+//            let calloutView = UIButton(frame: rect)
+//            let text = face.hasSmile ? "😀" : "😐"
+//            calloutView.titleLabel!.font = UIFont.systemFontOfSize(face.bounds.width)
+//            calloutView.setTitle(text, forState: UIControlState.Normal)
+            
+            var calloutView:UIImage!
+            
+            if face.hasSmile {
+                calloutView = UIImage(named:"happy.png")
+            } else {
+                calloutView = UIImage(named:"sad.png")
+            }
+            
+            
+            calloutView!.drawInRect(rect)
+            
+            
+            CGContextStrokeRect(drawCtxt,rect)
+//            self.imageView.addSubview(calloutView)
+            
+//            self.emojiArray.append(calloutView)
+            
+            
             
         }
+        let drawedImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        imageView.image = drawedImage
+    }
+    /////////////////////////////RESIZE FUNC/////////////////////////\
+    
+    func ResizeImage(image: UIImage, targetSize: CGSize) -> UIImage {
+        print("in resize functon")
+        let size = image.size
+        
+        let widthRatio  = targetSize.width  / image.size.width
+        let heightRatio = targetSize.height / image.size.height
+        
+        // Figure out what our orientation is, and use that to form the rectangle
+        var newSize: CGSize
+        if(widthRatio > heightRatio) {
+            newSize = CGSizeMake(size.width * heightRatio, size.height * heightRatio)
+        } else {
+            newSize = CGSizeMake(size.width * widthRatio,  size.height * widthRatio)
+        }
+        
+        // This is the rect that we've calculated out and this is what is actually used below
+        let rect = CGRectMake(0, 0, newSize.width, newSize.height)
+        
+        // Actually do the resizing to the rect using the ImageContext stuff
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
+        image.drawInRect(rect)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return newImage
     }
     
     
     @IBOutlet weak var pickFromCamera: UIButton!
     
     @IBAction func pickFromCamera(sender: AnyObject) {
-        var imagePickerController = UIImagePickerController()
+        for emoji in emojiArray {
+            emoji.removeFromSuperview()
+        }
+        emojiArray.removeAll()
+        let imagePickerController = UIImagePickerController()
         imagePickerController.delegate = self
         imagePickerController.sourceType = UIImagePickerControllerSourceType.Camera
         imagePickerController.allowsEditing = true
@@ -67,11 +232,16 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
 
         
     }
+
+    
     
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
         if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
-            imageView.image = pickedImage
+            let resized = self.ResizeImage(pickedImage, targetSize: CGSizeMake(self.imageView.frame.width, self.imageView.frame.height))
+            imageView.image = resized
             imageView.contentMode = .ScaleAspectFit
+            
+            print("get image")
             addEmoji()
         }
         dismissViewControllerAnimated(true, completion: nil)
@@ -80,7 +250,8 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
     
     override func didReceiveMemoryWarning() {
